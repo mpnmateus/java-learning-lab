ce55d8da693eee5c31a877b0b896dcaa7f56eabe
mpnmateus <mpnmateus@gmail.com>
2026-06-29 20:44:20 -0300

    aula 1 sobre tipos primitivos

---
+++ src/javalearninglab/maratonajava/introducao/aula02/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package javalearninglab.maratonajava.introducao.aula02;
+
+public class TiposPrimitivos {
+    public static void main(String[] args){
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 35;
+        System.out.println("A idade é: " + idade);
+    }
+}
