6572180aa4cea711d2df02e988621def8e6f61d2
mpnmateus <mpnmateus@gmail.com>
2026-06-30 10:11:11 -0300

    trabalhando o casting

--- src/javalearninglab/maratonajava/introducao/aula02/TiposPrimitivos.java
+++ src/javalearninglab/maratonajava/introducao/aula02/TiposPrimitivos.java
@@ -3,17 +3,19 @@ package javalearninglab.maratonajava.introducao.aula02;
 public class TiposPrimitivos {
     public static void main(String[] args){
         // int, double, float, char, byte, short, long, boolean
-        int idade = 35;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
-        byte idadeByte = 35;
-        short idadeShort = 35;
+        int idade = (int) 10000000000L; // Vai forçar para caber, mas vai cortar os bits para caber (não sabemos o que esperar)
+        long numeroGrande = (long) 155.23;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 127; //Vai até 127
+        short idadeShort = 32767; // Vai até 32767
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere= 'M';
 
         System.out.println("A idade é: " + idade);
         System.out.println(verdadeiro);
+        System.out.println(idade);
+        System.out.println(numeroGrande);
     }
 }
