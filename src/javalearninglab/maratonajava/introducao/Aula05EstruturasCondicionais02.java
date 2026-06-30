adbc71fd832a94ff211f8fa912f65b4e624f57ce
mpnmateus <mpnmateus@gmail.com>
2026-06-30 16:10:13 -0300

    trabalhando com else if e inicializacao de variaveis

---
+++ src/javalearninglab/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,20 @@
+package javalearninglab.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais02 {
+    public static void main(String[] args){
+        // idade < 15 caregoria infantil
+        // idade >= 15 && idade < 18 categoria juvenil
+        // idade >= 18 categoria adulto
+        int idade = 21;
+        String categoria; // variaveis de escopo local devem ser inicializadas antes de executá-las
+
+        if(idade < 15){
+            categoria = "Categoria infantil";
+        } else if (idade >= 15 && idade < 18){
+            categoria = "Categoria juvenil";
+        } else {
+            categoria = "Categoria adulto";
+        }
+        System.out.println("A categoria é: " + categoria);
+    }
+}
