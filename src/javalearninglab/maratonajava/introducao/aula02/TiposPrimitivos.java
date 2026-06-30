322f6cff36822ee4a237c6f5db8450124031ce37
mpnmateus <mpnmateus@gmail.com>
2026-06-29 21:01:42 -0300

    aula 2 sobre tipos primitivos: explorando os 8 demais.

--- src/javalearninglab/maratonajava/introducao/aula02/TiposPrimitivos.java
+++ src/javalearninglab/maratonajava/introducao/aula02/TiposPrimitivos.java
@@ -4,6 +4,16 @@ public class TiposPrimitivos {
     public static void main(String[] args){
         // int, double, float, char, byte, short, long, boolean
         int idade = 35;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 35;
+        short idadeShort = 35;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere= 'M';
+
         System.out.println("A idade é: " + idade);
+        System.out.println(verdadeiro);
     }
 }
