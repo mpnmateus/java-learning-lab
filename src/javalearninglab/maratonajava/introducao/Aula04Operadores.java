81a3136e60041e5b9cae28589003725fdfe8144a
mpnmateus <mpnmateus@gmail.com>
2026-06-30 14:14:35 -0300

    operadores aritmeticos

---
+++ src/javalearninglab/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,12 @@
+package javalearninglab.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args){
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultadoDivisao = numero01 / (double) numero02;
+        System.out.println(numero01+numero02+". A partir daqui concatena: "+numero01+numero02+"\n");
+        System.out.println(resultadoDivisao);
+    }
+}
