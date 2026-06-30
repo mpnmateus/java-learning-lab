a358a33cf54c69194a23177e7f8f69019bcb8247
mpnmateus <mpnmateus@gmail.com>
2026-06-30 14:45:24 -0300

    operadores logicos

--- src/javalearninglab/maratonajava/introducao/Aula04Operadores.java
+++ src/javalearninglab/maratonajava/introducao/Aula04Operadores.java
@@ -7,11 +7,24 @@ public class Aula04Operadores {
         int numero02 = 20;
         double resultadoDivisao = numero01 / (double) numero02;
         System.out.println(numero01+numero02+". A partir daqui concatena: "+numero01+numero02+"\n");
-        System.out.println(resultadoDivisao);
+        System.out.println("Resultado da divisão: " + resultadoDivisao);
+        System.out.println("---------------"+"\n");
 
         // Resto (%): se um numero dividido por 2 tem resto zero, ele é par
         int resto = 5 % 2;
-        System.out.println(resto);
+        System.out.println("resto: "+ resto);
+        System.out.println("---------------"+"\n");
 
+        // Operadores lógicos: < > <= >= == != (sempre retornam valores booleanos)
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isDezIgualDez " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
     }
 }
