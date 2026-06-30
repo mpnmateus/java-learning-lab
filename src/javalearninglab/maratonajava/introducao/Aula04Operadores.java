5da30fea4a6e42da680b4fbe50bc8faa06785629
mpnmateus <mpnmateus@gmail.com>
2026-06-30 14:22:17 -0300

    trabalhando com resto

--- src/javalearninglab/maratonajava/introducao/Aula04Operadores.java
+++ src/javalearninglab/maratonajava/introducao/Aula04Operadores.java
@@ -2,11 +2,16 @@ package javalearninglab.maratonajava.introducao;
 
 public class Aula04Operadores {
     public static void main(String[] args){
-        // + - / *
+        // Aritméticos: + - / *
         int numero01 = 10;
         int numero02 = 20;
         double resultadoDivisao = numero01 / (double) numero02;
         System.out.println(numero01+numero02+". A partir daqui concatena: "+numero01+numero02+"\n");
         System.out.println(resultadoDivisao);
+
+        // Resto (%): se um numero dividido por 2 tem resto zero, ele é par
+        int resto = 5 % 2;
+        System.out.println(resto);
+
     }
 }
