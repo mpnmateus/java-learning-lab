73ea0dc242700a344369ecb6f0e677ac666875dc
mpnmateus <mpnmateus@gmail.com>
2026-06-30 15:05:35 -0300

    operador logico: OR

--- src/javalearninglab/maratonajava/introducao/Aula04Operadores.java
+++ src/javalearninglab/maratonajava/introducao/Aula04Operadores.java
@@ -29,12 +29,24 @@ public class Aula04Operadores {
         System.out.println("---------------"+"\n");
 
         // && (and) || (or) ! (not)
+
+        // &&
         int idade = 35;
         float salario = 3500F;
         boolean isDentroDaLeiMaiorQueTrinta = (idade > 30) && (salario >= 4612);
         boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
         System.out.println("isDentroDaLeiMaiorQueTrinta: " + isDentroDaLeiMaiorQueTrinta);
         System.out.println("isDentroDaLeiMenorQueTrinta: " + isDentroDaLeiMenorQueTrinta);
+        System.out.println("---------------"+"\n");
+
+        // ||
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystationCinco = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente >= valorPlaystationCinco || valorTotalContaPoupanca >= valorPlaystationCinco;
+
+        System.out.println("O playstation é comprável: " + isPlaystationCincoCompravel);
+        System.out.println("---------------"+"\n");
 
     }
 }
