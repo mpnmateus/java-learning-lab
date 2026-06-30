cae806136ad3cca103f51367920b7cb4a0f8296a
mpnmateus <mpnmateus@gmail.com>
2026-06-30 15:59:36 -0300

    apresentando o else. alterando nome da classe

--- src/javalearninglab/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/javalearninglab/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,12 +1,16 @@
 package javalearninglab.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args){
         int idade = 15;
         boolean isAutorizadoComprarBebida = idade >= 18;
         if(isAutorizadoComprarBebida){
             System.out.println("Autorizado a comprar bebida alcoólica.");
+        } else {
+            System.out.println("Não autorizado a comprar bebida alcoólica");
         }
+
+
         if(!isAutorizadoComprarBebida){
             System.out.println("Não autorizado a comprar bebida alcoólica");
         }
