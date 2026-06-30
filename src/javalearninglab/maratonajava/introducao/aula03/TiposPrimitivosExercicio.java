519453ab44643b555775e63f20e88c5404671843
mpnmateus <mpnmateus@gmail.com>
2026-06-30 14:03:09 -0300

    trabalhando com variaveis, imprimindo-as na tela

---
+++ src/javalearninglab/maratonajava/introducao/aula03/TiposPrimitivosExercicio.java
@@ -0,0 +1,24 @@
+package javalearninglab.maratonajava.introducao.aula03;
+
+import java.util.Date;
+
+/*
+Prática
+
+Crie variáveis para os campos descritos abaixo entre <> e imprima a mensagem:
+Eu <nome>, morando no endereço <endereço>,
+confirmo que recebi o <salario> na data <data>
+* */
+public class TiposPrimitivosExercicio {
+    public static void main(String[] args){
+        String nome = "Mateus";
+        String endereco = "Avenida Mauá, 100";
+        double salario = 3500.50;
+        Date dataHoje = new Date();
+        String relatorio = "Eu "+ nome + ", morando no endereço "+ endereco + ",\n" +
+                "confirmo que recebi o salário de R$ "+ salario + " na data de hoje, " + dataHoje;
+
+        System.out.println(relatorio);
+    }
+
+}
