7e6fc38685b90d4e8ab027fa1b58b7f470d7d436
mpnmateus <mpnmateus@gmail.com>
2026-07-02 20:31:17 -0300

    iterando no array

--- src/javalearninglab/maratonajava/introducao/Aula07Arrays02.java
+++ src/javalearninglab/maratonajava/introducao/Aula07Arrays02.java
@@ -7,9 +7,28 @@ public class Aula07Arrays02 {
         // char '\u0000' == ' ' (espaço em branco)
         // boolean == false
         // String null
-        String [] nomes = new String[3];
-        System.out.println(nomes[0]);
-        System.out.println(nomes[1]);
-        System.out.println(nomes[2]);
+        String [] nomes01 = new String[3];
+        /*
+        System.out.println(nomes01[0]);
+        System.out.println(nomes01[1]);
+        System.out.println(nomes01[2]);
+        */
+
+        // ITERANDO NO ARRAY
+        // variaveis do tipo reference tem, entre outras coisas, "fields" (campo especial) - length
+        String [] nomes = new String[5];
+        nomes[0] = "Mateus";
+        nomes[1] = "João";
+        nomes[2] = "Maria";
+
+        for(int i = 0; i < nomes.length; i++){
+            System.out.println(nomes[i]);
+        }
+
+        // Se eu instanciar novamente, eu perco a referência anterior (garbage collector apaga o objeto anterior pois está sem referencia)
+        nomes = new String[6];
+        for(int i = 0; i < nomes.length; i++){
+            System.out.println(nomes[i]); // null
+        }
     }
 }
