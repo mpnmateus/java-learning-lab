83c5ad561bbe6f6cf4046b07dc5c71d5ae33f517
mpnmateus <mpnmateus@gmail.com>
2026-07-02 21:30:35 -0300

    arrays multidimensionais: array de arrays.

---
+++ src/javalearninglab/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,25 @@
+package javalearninglab.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+
+        // Árray de arrays
+        // Cada posição do array base, tem um array de 3 posições
+        int[][] dias = new int[3][3];
+        System.out.println(dias);
+
+        dias[0][0] = 40;
+        dias[0][1] = 23;
+        dias[0][2] = 32;
+
+        dias[1][0] = 45;
+        dias[1][1] = 50;
+        dias[1][2] = 24;
+
+        for(int i = 0; i < dias.length; i++){
+            for (int j = 0; j < dias[0].length; j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+    }
+}
