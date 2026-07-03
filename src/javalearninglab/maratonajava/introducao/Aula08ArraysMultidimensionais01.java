d49c23c37d9c1dbe803356033c1bbc14479cabcd
mpnmateus <mpnmateus@gmail.com>
2026-07-02 21:38:53 -0300

    refactor: corrigindo comprimento do array base para iterar

--- src/javalearninglab/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/javalearninglab/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -17,7 +17,7 @@ public class Aula08ArraysMultidimensionais01 {
         dias[1][2] = 24;
 
         for(int i = 0; i < dias.length; i++){
-            for (int j = 0; j < dias[0].length; j++){
+            for (int j = 0; j < dias[i].length; j++){
                 System.out.println(dias[i][j]);
             }
         }
