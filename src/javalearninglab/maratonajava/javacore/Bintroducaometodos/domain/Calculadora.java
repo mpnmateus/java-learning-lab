8af9ddbd290c21e70759068a3e40248d0e172534
mpnmateus <mpnmateus@gmail.com>
2026-07-06 20:34:11 -0300

    introducao a metodos

---
+++ src/javalearninglab/maratonajava/javacore/Bintroducaometodos/domain/Calculadora.java
@@ -0,0 +1,13 @@
+package javalearninglab.maratonajava.javacore.Bintroducaometodos.domain;
+
+public class Calculadora {
+
+    public void somarDoisNumeros(){
+        System.out.println(10+10);
+        //obs.: saida é diferente de retorno
+    }
+
+    public void subtraiDoisNumeros(){
+        System.out.println(20-10);
+    }
+}
---
+++ src/javalearninglab/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,14 @@
+package javalearninglab.maratonajava.javacore.Bintroducaometodos.test;
+
+import javalearninglab.maratonajava.javacore.Bintroducaometodos.domain.Calculadora;
+
+public class CalculadoraTest01 {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        calc.somarDoisNumeros();
+        calc.subtraiDoisNumeros();
+    }
+
+}
