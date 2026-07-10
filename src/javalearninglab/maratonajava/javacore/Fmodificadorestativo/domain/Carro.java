a1ed85757c6af0131ef972e05b2b8a999bcdf3b8
mpnmateus <mpnmateus@gmail.com>
2026-07-10 19:04:56 -0300

    metodos estaticos. qnd os metodos nao acessam um atributo da instancia

--- src/javalearninglab/maratonajava/javacore/Fmodificadorestativo/domain/Carro.java
+++ src/javalearninglab/maratonajava/javacore/Fmodificadorestativo/domain/Carro.java
@@ -1,11 +1,12 @@
 package javalearninglab.maratonajava.javacore.Fmodificadorestativo.domain;
 
 public class Carro {
+
     private String nome;
     private double velocidadeMaxima;
     // Com mod de acesso static, essa variavel nao pertence ao objeto, mas à classe
-    // Todos os objetos advindos dessa classe, compartilharão o valor alterado já que essa é uma variável de classe
-    public static double velocidadeLimite = 250;
+    // Todos os objetos derivados dessa classe (todas as instancias), compartilharão o valor alterado já que essa é uma variável de classe
+    private static double velocidadeLimite = 250;
 
     public Carro(String nome, double velocidadeMaxima){
         this.nome = nome;
@@ -16,31 +17,25 @@ public class Carro {
         System.out.println("--------");
         System.out.println("Nome: " + this.nome);
         System.out.println("Velocidade Máxima: " + this.velocidadeMaxima);
-        // como a variavel tem o modificador static, preciso acessá-la através da classe e não do atributo de um objeto
-        System.out.println("Velocidade Limite: " + Carro.velocidadeLimite);
+        // como a variavel tem o modificador static, é melhor acessá-la através da classe (Carro) e não do atributo de um objeto
+        System.out.println("Velocidade Limite: " + this.velocidadeLimite); // Mas como temos certeza que o métod é chamado por uma instância, vou manter o this
     }
+    // o this. não funciona porque existe a possibilidade da instancia não existir (já que o atributo é static)
+    // funciona com imprime pois ele não é estático e será chamado por uma instancia
+    public static void setVelocidadeLimite(double velocidadeLimite){
+        Carro.velocidadeLimite = velocidadeLimite; // Importante acessar via Carro., pois o java prioriza sempre o escopo mais próximo (o local) ;
+        // e, assim, teríamos uma atribuição da variável local (recebida por parametro) nela mesma (não na de classe)
+        // Obs.: nao posso acessar nenhuma variável não estática dentro de métodos estáticos
+    }
+    /* Criação de métodos estáticos
+     * Quando os métodos não acessam uma variável (um atributo) da instância
+     * */
 
-    public String getNome() {
-        return nome;
-    }
+    public static double getVelocidadeLimite(){ return velocidadeLimite; } // aqui funciona pois nao temos variavel recebida por parametro
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public double getVelocidadeMaxima() { return velocidadeMaxima; }
+    public void setVelocidadeMaxima(double velocidadeMaxima) { this.velocidadeMaxima = velocidadeMaxima; }
 
-    public double getVelocidadeMaxima() {
-        return velocidadeMaxima;
-    }
-
-    public void setVelocidadeMaxima(double velocidadeMaxima) {
-        this.velocidadeMaxima = velocidadeMaxima;
-    }
-
-    public double getVelocidadeLimite() {
-        return velocidadeLimite;
-    }
-
-    public void setVelocidadeLimite(double velocidadeLimite) {
-        this.velocidadeLimite = velocidadeLimite;
-    }
 }
--- src/javalearninglab/maratonajava/javacore/Fmodificadorestativo/test/CarroTest01.java
+++ src/javalearninglab/maratonajava/javacore/Fmodificadorestativo/test/CarroTest01.java
@@ -3,19 +3,20 @@ package javalearninglab.maratonajava.javacore.Fmodificadorestativo.test;
 import javalearninglab.maratonajava.javacore.Fmodificadorestativo.domain.Carro;
 
 public class CarroTest01 {
+    static String test;
     public static void main(String[] args) {
-//        Carro c1 = new Carro("BMW", 290);
-//        Carro c2 = new Carro("Ferrari", 310);
-//        //c1.setVelocidadeLimite(200);
+        Carro c1 = new Carro("BMW", 290);
+        Carro c2 = new Carro("Ferrari", 310);
+        c1.imprime();
+        c2.imprime();
 
-        // como tem modificador static, não acesso mais o atributo através de uma variável de referência, mas através da classe
-        // Na verdade, com isso, não existe mais a necessidade de eu ter objetos (comentei tudo)
-        System.out.println(Carro.velocidadeLimite); // 250
-        Carro.velocidadeLimite = 180;
-        System.out.println(Carro.velocidadeLimite); // 180
+        test = "TestandoOAcessoDeUmaVariavelEstaticaDentroDeUmMetodoEstatico";
 
-//        c1.imprime();
-//        c2.imprime();
+        Carro.setVelocidadeLimite(150);
+        c1.imprime();
+        c2.imprime();
+
+
 
     }
 }
