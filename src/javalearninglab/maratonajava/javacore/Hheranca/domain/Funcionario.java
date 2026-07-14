ed0d478a2cf5289d2ca82ada430ba9ce996f75e3
mpnmateus <mpnmateus@gmail.com>
2026-07-14 15:03:28 -0300

    modificador de acesso protected: possibilita acesso aos atributos para as subclasses e apenas para classes dentro do mesmo pacote

--- src/javalearninglab/maratonajava/javacore/Hheranca/domain/Funcionario.java
+++ src/javalearninglab/maratonajava/javacore/Hheranca/domain/Funcionario.java
@@ -7,6 +7,13 @@ public class Funcionario extends Pessoa{
         super.imprime();
         System.out.println(this.salario);
     }
+    public void relatorioPagamento(){
+        // this.nome está disponível apenas em tempo de execução e this.getNome() é má prática
+        // Por isso usaremos o modificador protected na sua super classe (possibilita acesso às sub classes e também dentro do pct, como se fossem publicos)
+        //System.out.println("Eu " + this.getNome() + " recebi o salário de R$ " + this.salario);
+        System.out.println("Eu " + this.nome + " recebi o salário de R$ " + this.salario);
+
+    }
     public double getSalario() {
         return salario;
     }
