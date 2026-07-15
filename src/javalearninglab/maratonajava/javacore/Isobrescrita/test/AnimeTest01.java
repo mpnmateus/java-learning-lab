d7b4a22230cee6df4f8256a720181382ea9af7fb
mpnmateus <mpnmateus@gmail.com>
2026-07-15 11:41:39 -0300

    trabalhando com @Override (sobrescrita) usando o metodo toString() para exemplificar

---
+++ src/javalearninglab/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package javalearninglab.maratonajava.javacore.Isobrescrita.test;
+
+import javalearninglab.maratonajava.javacore.Isobrescrita.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Sakura");
+        System.out.println(anime);
+    }
+}
