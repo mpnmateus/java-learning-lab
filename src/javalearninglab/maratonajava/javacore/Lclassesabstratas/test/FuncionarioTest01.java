7f1e7ba4748d30c856cf5cc8e46feb7b890798f5
mpnmateus <mpnmateus@gmail.com>
2026-07-15 14:11:49 -0300

    classes abstratas: feitas para serem estendidas (impedindo a criacao de objetos derivados dessas classes). As suas classes concretas a implementam.

---
+++ src/javalearninglab/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,14 @@
+package javalearninglab.maratonajava.javacore.Lclassesabstratas.test;
+
+import javalearninglab.maratonajava.javacore.Lclassesabstratas.domain.Desenvolvedor;
+import javalearninglab.maratonajava.javacore.Lclassesabstratas.domain.Funcionario;
+import javalearninglab.maratonajava.javacore.Lclassesabstratas.domain.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Janine", 8500);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Mateus", 10000);
+        System.out.println(desenvolvedor);
+        System.out.println(gerente);
+    }
+}
