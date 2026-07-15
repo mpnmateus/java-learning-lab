b3ab38bf318ad13e5a057ccf9b7d6856a16471b5
mpnmateus <mpnmateus@gmail.com>
2026-07-15 14:42:14 -0300

    metodos abstratos. sem corpo. servem de flag que obrigam as classes concretas a implementa-los

--- src/javalearninglab/maratonajava/javacore/Lclassesabstratas/domain/Funcionario.java
+++ src/javalearninglab/maratonajava/javacore/Lclassesabstratas/domain/Funcionario.java
@@ -7,6 +7,17 @@ public abstract class Funcionario {
     public Funcionario(String nome, double salario){
         this.nome = nome;
         this.salario = salario;
+        calculaBonus();
     }
+    public abstract void calculaBonus(); // Obriga a implementação para as suas classes concretas
 
+    // Em uma classe abstrata posso ter métodos abstratos e métodos não abstratos
+    // Porém, nunca poderei ter métodos abstratos em uma classe concreta
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
 }
--- src/javalearninglab/maratonajava/javacore/Lclassesabstratas/domain/Gerente.java
+++ src/javalearninglab/maratonajava/javacore/Lclassesabstratas/domain/Gerente.java
@@ -4,6 +4,10 @@ public class Gerente extends Funcionario{
     public Gerente(String nome, double salario){
         super(nome, salario);
     }
+    @Override
+    public void calculaBonus(){
+        this.salario = this.salario + this.salario * 0.1;
+    }
 
     @Override
     public String toString() {
