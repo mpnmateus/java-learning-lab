e1e5b5f5dc0300e8cc3afa74d9f5cc435ea98b7e
mpnmateus <mpnmateus@gmail.com>
2026-07-31 18:49:25 -0300

    somatorio e media usando while e if

---
+++ src/exercicios/bloco1/serie3/SomatorioEMedia.java
@@ -0,0 +1,40 @@
+package exercicios.bloco1.serie3;
+
+import java.util.Scanner;
+
+public class SomatorioEMedia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quantos números você quer informar: ");
+        int quantidade = sc.nextInt();
+
+        int contador = 1;
+        double somatorio = 0;
+        double numAnterior = 0;
+        double maior = 0;
+        double menor = 0;
+        while(contador <= quantidade){
+            System.out.println("Digite o " + contador + "º número: ");
+            double num = sc.nextDouble();
+            somatorio += num;
+
+            if((contador == 1) && (menor == 0)){
+                menor = num;
+            }
+            if ((num < numAnterior) && (num < menor)){
+                menor = num;
+            }
+            if((num > numAnterior) && (num > maior)){
+                maior = num;
+            }
+
+            numAnterior = num;
+            contador++;
+        }
+        double media = somatorio / quantidade;
+        System.out.println("\nSomatorio: " + somatorio);
+        System.out.println("Maior nº: " +maior);
+        System.out.println("Menor: " + menor);
+        System.out.println("Média: " + media);
+    }
+}
